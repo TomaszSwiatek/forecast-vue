81d91e2a6b223dddaf58a8092f9c6ea0b180845c
Tomasz Swiatek <tomasswiatek@gmail.com>
2019-01-31 10:46:30 +0100

    created simple search layout

---
+++ src/views/Search.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="wrapper">
+    <div class="search">
+      <label for="search">Search</label>
+      <input type="text" id="search" name="search">
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue';
+
+export default {
+  name: "search"
+};
+</script>
+<style lang="scss" scoped>
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  width: 100%;
+  margin: 0;
+  padding: 30px;
+  .search {
+    display: flex;
+    flex-direction: column;
+    width: 250px;
+  }
+  input {
+    height: 30px;
+    border: 0;
+    border-bottom: 1px solid black;
+  }
+  label {
+    font-family: Montserrat, sans-serif;
+  }
+}
+</style>
+
