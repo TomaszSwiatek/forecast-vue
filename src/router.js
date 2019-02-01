b0360b87ad6e0879ac4822e12b73d917ff340194
Tomasz Swiatek <tomasswiatek@gmail.com>
2019-02-01 21:03:53 +0100

    subsite about removed

--- src/router.js
+++ src/router.js
@@ -1,25 +1,15 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Search from './views/Search.vue';
-import About from './views/About.vue';
+
 
 Vue.use(Router);
 
 
 export default new Router({
   routes: [{
-      path: '/',
-      name: 'Search',
-      component: Search,
-    },
-    {
-      path: '/about',
-      name: 'About',
-      component: About,
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      // component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    },
-  ],
+    path: '/',
+    name: 'Search',
+    component: Search,
+  }],
 });
